c0c3bbd1e561a3ce480b9a8e20a64c2a201204e3
jiwoo <jwo2955@gmail.com>
2026-04-14 21:35:06 +0900

    docs: 결제 시스템을 즉시 결제(Instant Capture) 모델로 정의

--- src/main/java/com/january/ledgerflow/payment/service/PaymentService.java
+++ src/main/java/com/january/ledgerflow/payment/service/PaymentService.java
@@ -12,6 +12,15 @@ import com.january.ledgerflow.transaction.service.TransactionService;
 import lombok.AllArgsConstructor;
 import org.springframework.stereotype.Service;
 
+/**
+ * 국내 대부분의 이커머스가 사용하는 "즉시 결제" 모델을 구현한다.
+ * 국내 PG는 "결제 완료 = 매입 완료"를 보장하기 위해 승인과 매입을 하나로 묶는다.
+ *
+ * 즉, 결제 요청 시 PG사에서 카드사로의 결제 승인요청(approve)과 매입(capture)까지 한 번에 진행하며,
+ * 결제사(해당 프로그램) 입장에서는 항상 capture까지 완료된 결과를 받게 된다.
+ *
+ * 이로 인해 사실상 cancel과 refund가 동일한 프로세스를 진행하게 된다.
+ */
 @Service
 @AllArgsConstructor
 public class PaymentService {
